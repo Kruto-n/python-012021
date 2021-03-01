8ff8e35b0d872a94858fd272621758574c598916
Karin <Karin.Paurova.ff@gmail.com>
2021-03-01 21:34:03 +0100

    ukol 1.3

---
+++ 1/program03.py
@@ -0,0 +1,8 @@
+volnePokoje = {
+ 9: ["Amadeus", "Goya", "Vlasy"],
+ 10: ["Forman", "Goya"],
+ 11: [],
+ 12: ["Amadeus", "Vlasy"]
+}
+kdy = int(input("Na jakou hodinu chcete zamluvit meeting room?"))
+print(volnePokoje[kdy])
