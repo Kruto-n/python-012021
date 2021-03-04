fc8f0f31e13bc9a87727b2e1a5cf5e0093e5f662
Karin <Karin.Paurova.ff@gmail.com>
2021-03-04 21:40:26 +0100

    ukol 1.5 - opraven bool

--- 1/program01.py
+++ 1/program01.py
@@ -5,12 +5,9 @@ baliky = {
     "B501X": True,
     "B947X": False,
 }
-kod = 0
 kod = input("Jaky je kod vaseho baliku?")
 if kod in baliky:
-    if baliky[kod] == True:
-        print("Balík byl předán kurýrovi")
-    else:
-        print("Balík zatím nebyl předán kurýrovi")
-
-#Ráda bych se zeptala, proč v řádce 10 musí být if a ne for
+  if baliky[kod]:
+    print("Balík byl předán kurýrovi")
+  else:
+    print("Balík zatím nebyl předán kurýrovi")
