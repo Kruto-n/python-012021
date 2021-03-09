46a9a911b49f46d69ac19fb5eee3eacfc87bb8b1
Karin <Karin.Paurova.ff@gmail.com>
2021-03-09 22:42:11 +0100

    ukol 2.3 - funkcni

--- 2/program08.py
+++ 2/program08.py
@@ -3,19 +3,21 @@ number = number.replace(" ", " ")
 def validate(number):
     if len(number) == 9:
         print("V poradku.")
+        return True
     elif len(number) == 13:
         if number.startswith("+420"):
             print("V poradku.")
+            return True
     else:
         print("Cislo neni v poradku.")
-validate(number)
-message = input("Jaky je text zpravy?")
-def price(message):
-    lengthofmessage = len(message)
-    coefficient = lengthofmessage // 180
-    priceofmessage = coefficient * 3
-    if priceofmessage == 0:
-        print(f"Cena vasi zpravy je 3 Kc.")
-    else:
+        return False
+vporadku = validate(number)
+message = 0
+if vporadku:
+    def price(message):
+        message = input("Jaky je text zpravy?")
+        lengthofmessage = len(message)
+        coefficient = lengthofmessage // 180 + 1
+        priceofmessage = coefficient * 3
         print(f"Cena vasi zpravy je {priceofmessage} Kc.")
 price(message)
