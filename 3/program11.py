a8f00775a2f23b0d0b09d017a84422c00693a161
Karin <Karin.Paurova.ff@gmail.com>
2021-03-13 23:15:32 +0100

    ukol 3.1 - bez getInfo

--- 3/program11.py
+++ 3/program11.py
@@ -4,8 +4,6 @@ class Auto:
         self.typ = typ
         self.kilometry = kilometry
         self.dostupnost = dostupnost
-    def getInfo(self):
-        return f"Auto ma registracni znacku {self.registracniznacka}, typ {self.typ}, najeto {self.kilometry} a dostupnost je {self.dostupnost}."
+
 peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", "47534" )
 skoda = Auto("1P3 4747", "Škoda Octavia", "41253")
-print(peugeot.getInfo())
