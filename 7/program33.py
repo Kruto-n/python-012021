b17dcaa77e82ab46f14680e3ee345e929cdb158b
Karin <Karin.Paurova.ff@gmail.com>
2021-04-17 16:09:10 +0200

    ukol 7.2

---
+++ 7/program33.py
@@ -0,0 +1,9 @@
+import wget
+import pandas
+import matplotlib.pyplot as plt
+
+#wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/6/platy_2021_02.csv")
+
+data = pandas.read_csv("platy_2021_02.csv")
+data["plat"].hist(bins=[30000, 35000, 40000, 45000, 50000, 55000, 60000])
+plt.show()
