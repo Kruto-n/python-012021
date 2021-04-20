6c43bd3a28ace32b26f820ca51095794f6ad8a71
Karin <Karin.Paurova.ff@gmail.com>
2021-04-20 15:34:17 +0200

    ukol 6.1 / funkcni

--- 6/program26.py
+++ 6/program26.py
@@ -1,18 +1,29 @@
 import wget
-import pandas
+import pandas as pd
 
 #wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/6/zam_praha.csv")
 #wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/6/zam_plzeň.csv")
 #wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/6/zam_liberec.csv")
 #wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/6/platy_2021_02.csv")
 
-liberec = pandas.read_csv("zam_liberec.csv")
-plzen = pandas.read_csv("zam_plzeň.csv")
-praha = pandas.read_csv("zam_praha.csv")
-platy = pandas.read_csv("platy_2021_02.csv")
-print(plzen.head)
+liberec = pd.read_csv("zam_liberec.csv")
+plzen = pd.read_csv("zam_plzeň.csv")
+praha = pd.read_csv("zam_praha.csv")
+platy = pd.read_csv("platy_2021_02.csv")
+print(plzen.head())
 
 liberec['mesto'] = 'Liberec'
 plzen['mesto'] = 'Plzen'
 praha['mesto'] = 'Praha'
+
+zamestnanci = pd.concat([liberec, plzen, praha], ignore_index=True)
+zamestnanci.to_csv('zamestnanci.csv', index=False)
+
+zamestnanci_platy = pd.DataFrame.merge(zamestnanci, platy, on="cislo_zamestnance", how="outer")
+
+print(zamestnanci.shape) #(56, 5)
+print(platy.shape) #(43, 2)
+print(zamestnanci_platy.shape) #(56, 6)
+
+print(zamestnanci_platy.groupby('mesto')['plat'].mean())
 
